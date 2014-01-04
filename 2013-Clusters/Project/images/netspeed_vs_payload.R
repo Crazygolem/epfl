2c10d98583a59f32a039529afccf671ba6299b05
Crazygolem <crazygolem@gmail.com>
2014-01-05 00:06:33 +0100

    Network speed graph script done ("sum" aggregation)

--- 2013-Clusters/Project/images/netspeed_vs_payload.R
+++ 2013-Clusters/Project/images/netspeed_vs_payload.R
@@ -9,13 +9,12 @@ d <- read.csv('../data/communications_rtt_4.dat', sep=' ')
 
 # Filtering data
 d <- d[d$payload_size > 0,] # True RTT (no payload) cannot tell about speed
-#d <- d[d$num_messages == 1,]
 d$conc_messages <- d$num_messages # Messages sent concurrently (not aggregated)
 d$message_id <- NULL
 d$arrival_rank <- NULL
 
 # Aggregation
-d <- ddply(d, .(payload_size, delay, conc_messages), colwise(mean))
+d <- ddply(d, .(payload_size, delay, conc_messages), colwise(sum))
 # We cannot compute actual payload size here, as R uses
 # 32-bit integers and the total payload might be too large.
 #d$payload_size <- d$payload_size * d$num_messages
@@ -26,32 +25,42 @@ d$hrtt <- d$rtt_ms / 2
 d$netspeed <- (d$payload_size * 32) / (d$hrtt / 1000) * d$num_messages
 
 # Plotting function
-myplot <- function(df, outname) {
+myplot <- function(df, outname, log = 'x') {
   outfile <- paste0(outname, '.svg')
-  svg(outfile, width=10, height=8)
+  svg(outfile, width = 10, height = 8)
+
+  # Helper function to properly label axes with log scale
+  lab <- function(label, axis) {
+    return (paste(label, if (grepl(axis, log)) '(log scale)' else '', sep = ' '))
+  }
 
   # Scatterplot
   plot(df$payload_size, df$netspeed,
+    log=log,
     col=as.numeric(df$delay),
     pch=as.numeric(df$delay),
-    main="Network speed",
-    xlab="Payload size [# of integers]",
-    ylab="Network speed [b/s]")
+    main='Network speed',
+    xlab=lab('Payload size [# of integers]', 'x'),
+    ylab=lab('Network speed [b/s]', 'y')
+  )
 
   # Labels near points
   textxy(df$payload_size, df$netspeed, df$conc_messages,
     cex=0.8,
-    offset=1.2)
+    offset=1.2
+  )
 
   # Legend
-  legend("topright",
+  legend('topleft',
     levels(df$delay),
     pch=as.numeric(levels(factor(as.numeric(df$delay)))),
     col=as.numeric(levels(factor(as.numeric(df$delay)))),
-    title='Load management')
+    title='Load delay'
+  )
 
   dev.off()
 }
 
 # Last filtering and plotting
 myplot(d, 'netspeed_vs_payload')
+myplot(d, 'netspeed_vs_payload_logy', 'xy')
